2d1835dd65d411bb206bc5f4554b57729d4356cc
sandhiyasezhiyan <84834997+sandhiyasezhiyan@users.noreply.github.com>
2024-03-20 15:39:35 +0530

    Add files via upload

--- Button.java
+++ Button.java
@@ -10,34 +10,30 @@ import org.openqa.selenium.support.Color;
 
 public class Button {
 public static void main(String[] args) {
-	ChromeDriver driver = new ChromeDriver();
+	ChromeDriver driver=new ChromeDriver();
 	driver.get("https://leafground.com/button.xhtml");
 	driver.manage().window().maximize();
-	System.out.println(driver.findElement(By.xpath("(//span[text()='Click']/following::div)[3]")).isDisplayed());
-	driver.navigate().to("https://leafground.com/dashboard.xhtml;jsessionid=node0hr5el6cvg5ky11hcwjxak79k3433223.node0");
+	driver.findElement(By.xpath("//span[text()='Click']")).click();
+	System.out.println("The title is :"+driver.getTitle());
+	driver.navigate().back();
+	
+
+	//driver.findElement(By.xpath("(//div[@class='card'])[2]")).isDisplayed();
+	System.out.println("Disabled"+driver.getTitle());
+	driver.findElement(By.xpath("(//span[@class='ui-button-text ui-c'])[3]")).click();
 	System.out.println(driver.findElement(By.xpath("(//div[@class='card'])[2]")).isEnabled());
-	
-	WebElement ele = driver.findElement(By.xpath("//span[text()='Submit']"));
-	Point p = ele.getLocation();
-	System.out.println(p.getX());
-	System.out.println(p.getY());
-	
-	
-	
-	String buttoncolor = driver.findElement(By.xpath("//span[text()='Save']")).getCssValue("Save");
-	System.out.println("buttoncolor:"+buttoncolor);
-	
 	WebElement SubmitBtn=driver.findElement(By.xpath("(//span[text()='Submit'])[2]"));
 	org.openqa.selenium.Dimension size = SubmitBtn.getSize();
 	System.out.println(size.getHeight());
 	System.out.println(size.getWidth());
+	
+
+	String buttoncolor = driver.findElement(By.xpath("//span[text()='Save']")).getCssValue("Save");
+	System.out.println("buttoncolor:"+buttoncolor);
+
+
+	System.out.println(driver.findElement(By.xpath("//span[text()='Save']")).getCssValue("background"));
+
 	driver.close();
-	
-	
-	
-	
-	
-	
-	//System.out.println(driver.findElement(By.xpath("//span[text()='Save']")).getCssValue("#607d8b"));
 }
 }
